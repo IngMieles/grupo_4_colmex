86e53e993b2193c6e6741f41df6cb419ce105973
IngMieles <ingpablomieles@gmail.com>
2022-05-01 20:38:43 -0500

    estructura de carpetas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
-let express = require('express');
+const express = require('express');//
+const path = require('path');//Unificar las rutas dentro de los distintos OS para identificar donde nos ubicamos y a donde queremos llegar
+const app = express();//
+const publicPath = path.resolve(__dirname, './public');//Dirname hace referencia a la ubicacion del archivo app, le indico que busque desde la raiz que busque la carpeta public
 
-let app = express();
+app.use(express.static(publicPath))//Utilizamos la funcion static del objeto express, recibe la ruta de la carpeta donde se encuentran los archivos publicos
 
 app.listen(3001, () => console.log("Servidor corriendo en puerto 3001"));
 
 app.get('/', function(req, res){
-    res.send('Bienvenidos al sitio');
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
