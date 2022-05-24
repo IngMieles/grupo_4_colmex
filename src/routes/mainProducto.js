834b212c85253618aa79c078a041055a97db98b7
LAAngeles <76764765+LAAngeles@users.noreply.github.com>
2022-05-23 20:47:27 -0500

    nuevo archivo llamado mainProducto

---
+++ src/routes/mainProducto.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const mainController = require('../controllers/mainController'); 
+
+const router = express.Router();
+
+router.get('/:id', mainController.detalleProducto);
+
+
+module.exports = router;
