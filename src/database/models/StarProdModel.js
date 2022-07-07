1cc2804b61a8660dd02a7f68f6cf6056a04e3176
LAAngeles <76764765+LAAngeles@users.noreply.github.com>
2022-07-06 19:58:46 -0500

    se agrega un nuevo archivo

---
+++ src/database/models/StarProdModel.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = 'StarProdModel';
+    const cols = {
+        id : {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: dataTypes.STRING,
+        precio: dataTypes.FLOAT,
+        categoria: dataTypes.STRING,
+        img: dataTypes.TEXT,
+        fileImg: dataTypes.TEXT,
+        description: dataTypes.STRING,
+        userId: dataTypes.INTEGER
+    };
+
+    const config = {
+        tableName: "famous",
+        timestamps: false
+    };
+
+    const StarProdModel = sequelize.define(alias, cols, config);
+    return StarProdModel;
+}
