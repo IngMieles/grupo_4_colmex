9a42b2fa91285eecd39d2dde57e238076b109426
LAAngeles <76764765+LAAngeles@users.noreply.github.com>
2022-07-07 19:10:57 -0500

    se agrega .env para configurar las variables de entorno

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "colmex_db",
-    "host": "127.0.0.1",
+    // "username": "root",
+    "username": process.env.DB_USER,
+    // "password": null,
+    "password": process.env.DB_PASSWORD,
+    // "database": "colmex_db",
+    "database": process.env.DB_NAME,
+    // "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
